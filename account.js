1ff4113a076fdb99b7d33a2701557c46abc91428
abraham-yusuf <1703020028@students.unis.ac.id>
2021-06-13 13:15:58 +0700

    Create account.js
    2. Create & Fund a testnet Tezos account

---
+++ account.js
@@ -0,0 +1,25 @@
+let FAUCET_KEY;
+export default FAUCET_KEY ={
+    "mnemonic": [
+        "arena",
+        "top",
+        "modify",
+        "cotton",
+        "sail",
+        "same",
+        "usual",
+        "roof",
+        "olive",
+        "place",
+        "crash",
+        "piano",
+        "source",
+        "thank",
+        "crime"
+    ],
+    "secret": "4a9c4681c44a2619baa1bfd1054c7a7f85c47ac4",
+    "amount": "10207974611",
+    "pkh": "tz1byADNZPpVQ5jp5QDjikTbsmjf7fNRgbem",
+    "password": "UrSmTl1n9S",
+    "email": "xrfoqogv.mlwqgrnh@tezos.example.org"
+}
